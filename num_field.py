bf211c397c20dad115d805b07e61c68c5177290c
gefupi <gefupi@cs.tu-berlin.net>
2013-08-25 18:22:17 +0200

    fixed private name and changed type to set.

--- num_field.py
+++ num_field.py
@@ -1,7 +1,7 @@
 class num_field(object):
 
     def __init__(self):
-        self.__could_be_number__ = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self._could_be_number_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
         self.is_solved = False
 
     def __str__(self):
