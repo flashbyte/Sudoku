ad687ad76467e7c2c5e9fda48127fe86d97e82e2
Nils Mull <mail@flash-byte.de>
2013-08-26 00:36:31 +0200

    implemented brocken block_scanning

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import sudoku_read
 import logging
+my_suduko = sudoku_read.read_soduko_from_file('/tmp/some_file')
+
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
-    my_suduko = sudoku_read.read_soduko_from_file('/tmp/some_file')
     print ('???Sudoku to Solve???')
     print (my_suduko)
     my_suduko.solve()
--- sudoku_field.py
+++ sudoku_field.py
@@ -36,6 +36,24 @@ class sudoku_field(object):
         ret += "+-----------------------+\t+-----------------------+"
         return ret
 
+    def _get_block_as_list(self, block_id):
+        block_hash = {
+            1: (0, 0),
+            2: (0, 3),
+            3: (0, 6),
+            4: (3, 0),
+            5: (3, 3),
+            6: (3, 6),
+            7: (6, 0),
+            8: (6, 3),
+            9: (6, 6)
+        }
+        my_list = []
+        for row in range(block_hash[block_id][0], block_hash[block_id][0]+3):
+            for col in range(block_hash[block_id][1], block_hash[block_id][1]+3):
+                my_list.append(self._field[row][col])
+        return my_list
+
     def validate(self):
         valid = True
         #Validate rows
@@ -76,14 +94,23 @@ class sudoku_field(object):
             befor = str(self)
             if self._remove_possibilities():
                 changed = True
+            if not self.validate():
+                logging.debug(befor)
+                logging.debug("remove messed up")
+                logging.debug(str(self))
+                sys.exit(2)
+
             befor = str(self)
+            befor_set = str(self._field[6][5].get_set())
             if self._scanning():
                 changed = True
             if not self.validate():
                 logging.debug(befor)
+                logging.debug(befor_set)
                 logging.debug("scanning messed up")
                 logging.debug(str(self))
                 sys.exit(2)
+
             if self.apply():
                 changed = True
 
@@ -105,6 +132,16 @@ class sudoku_field(object):
                         result = True
         return result
 
+    def _remove_possibilities_from_block(self, block_id):
+        result = False
+        my_list = self._get_block_as_list(block_id)
+        for element in my_list:
+            if element.is_solved():
+                for element_remove in my_list:
+                    if element_remove.remove_posibility(element.get_num()):
+                        result = True
+        return result
+
     def _remove_possibilities(self):
         result = False
         for row in range(9):
@@ -113,7 +150,9 @@ class sudoku_field(object):
         for col in range(9):
             if self._remove_possibilities_from_col(col):
                 result = True
-        #Todo: remove from Block
+        for block in range(1, 10):
+            if self._remove_possibilities_from_block(block):
+                result = True
         return result
 
     def _scanning_row(self, row):
@@ -160,14 +199,42 @@ class sudoku_field(object):
 
         return True
 
+    def _scanning_block(self, block_id):
+        my_list = self._get_block_as_list(block_id)
+        #Make Union
+        my_union = set()
+        for element in my_list:
+            my_union = my_union.union(element.get_set())
+        #make intersec list
+        intersec_list = []
+        for element in my_list:
+            for element_intersect in my_list:
+                if element == element_intersect:
+                    continue
+                intersec_list.append(element.get_set() & element_intersect.get_set())
+        #remove intesect
+        for element in intersec_list:
+            my_union = my_union - element
+
+        if len(my_union) == 0:
+            return False
+
+        for uniq in my_union:
+            for element in my_list:
+                if uniq in element.get_set():
+                    element.set_num(uniq)
+
+        return True
+
     def _scanning(self):
-        result = False
         # Scanning Rows
         for row in range(9):
             if self._scanning_row(row):
-                result = True
+                return True
         for col in range(9):
             if self._scanning_col(col):
-                result = True
-        #TODO solve blocks
-        return result
+                return True
+        for block in range(1, 10):
+            if self._scanning_block(block):
+                return True
+        return False
