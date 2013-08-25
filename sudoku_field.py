bf9d47ea61f1085c3b9fed7386d5975d527a47cf
Nils Mull <mail@flash-byte.de>
2013-08-25 18:32:19 +0200

    Fixed init

--- sudoku_field.py
+++ sudoku_field.py
@@ -5,16 +5,20 @@ class sudoku_field(object):
     """simple"""
 
     def __init__(self):
-
-        self.__filed__ = [[num_field.num_field()] * 9] * 9
+        self._filed_ = []
+        for rows in range(9):
+            row = []
+            for cols in range(9):
+                row.append(num_field.num_field())
+            self._filed_.append(row)
 
     def set_field(self, row, col, value):
-        self.__filed__[row][col].is_solved = True
-        self.__filed__[row][col].__could_be_number__ = [value]
+        self._filed_[row][col].is_solved = True
+        self._filed_[row][col].__could_be_number__ = [value]
 
     def __str__(self):
         ret = ""
-        for line in self.__filed__:
+        for line in self._filed_:
             tmp = ""
             for i in line:
                 tmp = tmp + str(i) + ' '
