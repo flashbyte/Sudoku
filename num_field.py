4d10b77f8da3a938dfcf85a5d8cb16addd1be027
Nils Mull <mail@flash-byte.de>
2013-08-25 18:50:04 +0200

    Added setter

--- num_field.py
+++ num_field.py
@@ -9,3 +9,12 @@ class num_field(object):
             return str(self.__could_be_number__[0])
         else:
             return '*'
+
+    def set_num(self, value):
+        self._could_be_number_ = [value]
+        self.is_solved = True
+
+    def remove_posibility(self, value):
+        self._could_be_number_.remove(value)
+        if len(self._could_be_number_) == 1:
+            self.is_solved = True
