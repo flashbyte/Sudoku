fb702ccab9f3fe262ccaa99cfb603b2af91f01d7
gefupi <gefupi@cs.tu-berlin.net>
2013-08-25 18:07:19 +0200

    typo

--- num_field.py
+++ num_field.py
@@ -2,10 +2,10 @@ class num_field(object):
 
     def __init__(self):
         self.__could_be_number__ = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        self.is_soleved = False
+        self.is_solved = False
 
     def __str__(self):
-        if self.is_soleved:
+        if self.is_solved:
             return str(self.__could_be_number__[0])
         else:
             return '*'
