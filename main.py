09ea79aecf7bc7cd34dd1ff2976a2ca5e48f44a7
Nils Mull <mail@flash-byte.de>
2013-08-28 22:38:09 +0200

    Added argparser and listing of testcases

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python
 import logging
+import argparse
 from sudoku_reader import read_soduko_from_testcase
-
+from testcases import sudoku_testcases
 
 my_suduko = read_soduko_from_testcase(1)
 
 
+def init():
+    parser = argparse.ArgumentParser(description='Solves Sudokus')
+    parser.add_argument('-d', '--debug', dest='debug', action='store_true', help='Enables Debuging')
+    parser.add_argument('-lt', '--list', dest='list_testcases', action='store_true', help='Liste availbe Sudoku Testcases')
+
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig()
+
+    if args.list_testcases:
+        for testcases_id in range(len(sudoku_testcases)):
+            testc = read_soduko_from_testcase(testcases_id)
+            print ("---- %s ----" % (testc.description))
+            print (testc)
+
+
 def main():
-    logging.basicConfig(level=logging.DEBUG)
-    print('Solving: %s' % (my_suduko.description))
-    print(my_suduko)
-    my_suduko.solve()
-    print(my_suduko)
+    init()
 
 if __name__ == '__main__':
     main()
