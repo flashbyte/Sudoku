ef1ef78104c61d40685b15aa0e6a708be9f675e9
Nils Mull <mail@flash-byte.de>
2013-08-28 23:19:06 +0200

    More commandline features

--- main.py
+++ main.py
@@ -4,30 +4,46 @@ import argparse
 from sudoku_reader import read_soduko_from_testcase
 from testcases import sudoku_testcases
 
-my_suduko = read_soduko_from_testcase(1)
 
-
-def init():
+def main():
+    my_suduko = None
     parser = argparse.ArgumentParser(description='Solves Sudokus')
     parser.add_argument('-d', '--debug', dest='debug', action='store_true', help='Enables Debuging')
     parser.add_argument('-lt', '--list', dest='list_testcases', action='store_true', help='Liste availbe Sudoku Testcases')
-
+    parser.add_argument('sudoku', nargs='?', help='Sudoku to solve. Could be a Testace number or filename to valid Sudokufile')
     args = parser.parse_args()
 
+    # Debuging?
     if args.debug:
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig()
 
+    # List Testcases
     if args.list_testcases:
         for testcases_id in range(len(sudoku_testcases)):
             testc = read_soduko_from_testcase(testcases_id)
             print ("---- %s ----" % (testc.description))
             print (testc)
 
+    # Sudoku file/number
+    if args.sudoku:
+        if args.sudoku.isdigit():
+            if (int(args.sudoku) > -1) and (int(args.sudoku) < len(sudoku_testcases)):
+                my_suduko = read_soduko_from_testcase(int(args.sudoku))
+            else:
+                logging.error('Testcase %s not found', args.sudoku)
 
-def main():
-    init()
+        # TODO: Implement file read
+
+    if my_suduko:
+        print('Solving:\n%s' % my_suduko)
+        my_suduko.solve()
+        if my_suduko.is_solved():
+            print('Solved:\n%s' % my_suduko)
+        else:
+            print(';-( I am not smart enougth to solve this. Got so far:\n%s' % my_suduko)
+
 
 if __name__ == '__main__':
     main()
