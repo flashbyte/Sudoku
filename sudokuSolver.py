2c75715319c473ddde0d958e69f1209daf26b14d
Nils Mull <nils.mull@kaufda.de>
2013-12-22 20:27:45 +0100

    New parsing feature for sudoku files

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import logging
 import argparse
-from sudoku_reader import read_soduko_from_testcase
+import os
+from sudoku_reader import read_soduko_from_testcase, read_sudoku_from_file
 from testcases import sudoku_testcases
 
 
@@ -9,6 +10,7 @@ def main():
     my_suduko = None
     parser = argparse.ArgumentParser(description='Solves Sudokus')
     parser.add_argument('-d', '--debug', dest='debug', action='store_true', help='Enables Debuging')
+    parser.add_argument('-f', '--filename', dest='filename', help='Filename')
     parser.add_argument('-lt', '--list', dest='list_testcases', action='store_true', help='Liste availbe Sudoku Testcases')
     parser.add_argument('sudoku', nargs='?', help='Sudoku to solve. Could be a Testace number or filename to valid Sudokufile')
     args = parser.parse_args()
@@ -28,12 +30,23 @@ def main():
 
     # Sudoku file/number
     if args.sudoku:
-        if args.sudoku.isdigit():
-            if (int(args.sudoku) > -1) and (int(args.sudoku) < len(sudoku_testcases)):
-                my_suduko = read_soduko_from_testcase(int(args.sudoku))
+        if not args.filename:
+            if args.sudoku.isdigit():
+                if (int(args.sudoku) > -1) and (int(args.sudoku) < len(sudoku_testcases)):
+                    my_suduko = read_soduko_from_testcase(int(args.sudoku))
+                else:
+                    logging.error('Testcase %s not found', args.sudoku)
+        else:
+            if os.path.isfile(args.filename):
+                if args.sudoku == 0:
+                    pass # TODO: Parse all sudokus
+                else:
+                    if args.sudoku < 0:
+                        logging.error('Jow! Just positives numbers!')
+                    else:
+                        my_suduko = read_sudoku_from_file(os.path.abspath(args.filename), int(args.sudoku))
             else:
-                logging.error('Testcase %s not found', args.sudoku)
-
+                logging.error('File %s not found! fuc$$!!!' % (os.path.abspath(args.filename)))
         # TODO: Implement file read
 
     if my_suduko:
--- sudoku_reader.py
+++ sudoku_reader.py
@@ -8,3 +8,29 @@ def read_soduko_from_testcase(testcase_number):
         sudoku_testcases[testcase_number]['testcase']
     )
     return my_sudoku
+
+def read_sudoku_from_file(filename, sudoku_number):
+    file_handler = open(filename)
+    sudoku_list = file_handler.readlines()
+    file_handler.close()
+    sudoku_string = sudoku_list[sudoku_number].rstrip()
+    filed_tmp = [
+        sudoku_string[0:9],
+        sudoku_string[9:18],
+        sudoku_string[18:27],
+        sudoku_string[27:36],
+        sudoku_string[36:45],
+        sudoku_string[45:54],
+        sudoku_string[54:63],
+        sudoku_string[63:72],
+        sudoku_string[72:91]
+    ]
+    field = []
+    for row in range(len(filed_tmp)):
+        field.append([int(c) for c in filed_tmp[row]])
+    my_sudoku = sudoku_field.sudoku_field(
+        sudoku_string,
+        field
+    )
+    return my_sudoku
+
